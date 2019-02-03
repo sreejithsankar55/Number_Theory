012db6f3ac16586f1e8fa60c607fe9aa5f8f8857
Sreejith S <32271222+sreejithsankar55@users.noreply.github.com>
2019-02-03 19:23:05 +0530

    Create gcd.cpp

---
+++ gcd.cpp
@@ -0,0 +1,17 @@
+#include < bits / stdc++.h >
+using namespace std;
+int gcd(int a, int b) {
+  if (a > b)
+    swap(a, b);
+  if (a == 0)
+    return b;
+  else
+    return gcd(b % a, a);
+}
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << gcd(a, b);
+  return 0;
+}
